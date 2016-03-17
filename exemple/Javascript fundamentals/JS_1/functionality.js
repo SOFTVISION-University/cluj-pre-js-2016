b5926b3b4350d7414d2c502f3d03bce2df2e4b93
Adrian Orban <adrian.orban@softvision.ro>
2016-03-17 13:53:31 +0200

    half day js1

--- exemple/Javascript fundamentals/JS_1/functionality.js
+++ exemple/Javascript fundamentals/JS_1/functionality.js
@@ -1,9 +1,9 @@
 var i,
     s = '';
 
-// for (i = 0;i < 10000000;i++) {
-//   s = s + 'test';
-// }
+ //for (i = 0;i < 10000000;i++) {
+  // s = s + 'test';
+ //}
 
 //document.addEventListener('DOMContentLoaded', function() {
   var notifier = document.getElementById('notifier')
