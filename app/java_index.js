8d6710c45e125f9e8706251d534e933a51a845db
Cosmin ionascu <cosmin.ionascu@softvision.com>
2016-03-18 08:47:05 +0200

    Modifed the index file

---
+++ app/java_index.js
@@ -0,0 +1,14 @@
+
+
+
+
+function move() {
+console.log(document.getElementById("pop_playlist1"));
+document.getElementById("pop_playlist1").style.transition="0.5s";
+document.getElementById("pop_playlist1").style.height="600px";
+}
+function close() {
+console.log(document.getElementById("pop_playlist1"));
+document.getElementById("pop_playlist1").style.transition="0.5s";
+document.getElementById("pop_playlist1").style.height="0px";
+}
