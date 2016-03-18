08bcb652f415654bf763f85aeaece0aceb328361
Robert Cosma <robert.cosma@softvision.ro>
2016-03-18 10:42:33 +0200

    workday 5 commit
    commit 1 day 5
    
    created links between pages
    
    commit 4 day 5
    
    added focus effect on register page
    
    commit 6 day 5
    
    commit 7 day 5
    
    commit 8 day 5
    
    finished sign-in checks

--- app/register.js
+++ app/register.js
@@ -1,5 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('second-button').onclick=function(){
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+
+  function signupfree(){
+
+    document.getElementById('second-button').onclick=function(){
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+      document.forms['form'].elements['firstname'].focus();
+    }
+
   }
-});
+
+  
