24090c032c7c40d87edd5eed62f4486f9c6c14ab
Robert Cosma <robert.cosma@softvision.ro>
2016-03-18 11:43:42 +0200

    commit 2 day 5(on master)

--- app/signin.js
+++ app/signin.js
@@ -10,4 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('popup').style.visibility="hidden";
   }
 
+  document.getElementById('signin').onclick=function(){
+    document.location.href="file:///home/robert.cosma/work/cluj-pre-js-2016/app/login.html";
+  }
+
 });
