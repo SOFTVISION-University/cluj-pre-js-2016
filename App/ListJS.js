f28c7b1d04ad3b089c8a9fcfa5fbb1d42831d052
Adrian Orban <adrian.orban@softvision.ro>
2016-03-22 18:55:51 +0200

    add observer, reorganize classes, reorganize functions

--- App/ListJS.js
+++ App/ListJS.js
@@ -1,140 +1,80 @@
-var jsonObj = {
-    playlist:
-           {
-            picture1: "../core/assets/massive-pop-hits.png",
-            title1: "text1",
-            description:"text2",
-            songs:
-            [
-                "value1",
-                "value2",
-                "value3",
-                "value4",
-                "value5"
-            ],
-            songPict:["../core/assets/beats-like-birds.png",
-            "../core/assets/chill-vibes.png",
-            "../core/assets/indie-rocker.png",
-            "../core/assets/random-playlist.png",
-            "../core/assets/beats-like-birds.png"]
-
-
-        }
-}
-var jsonUserName={
-      name:"username",
-      password:"password"
-}
-
 var playListFromJS= JSON.parse(playlistsJSON);
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    var path = jsonObj.playlist.picture1;
-
-    document.getElementById('playL1').style.backgroundImage='url("'+path+'")';
-    var a=  Array.prototype.slice.call(jsonObj);
+    createPlayLists();
 
   });
-// playlist class
-function Playlist(data) {
-  this.playlistName="test";
-  this.image= data.image;
-  this.songsList=[];
-}
-Playlist.prototype.setName= function(name){
-  this.playlistName = name;
-}
-Playlist.prototype.addSong= function(song){
-    this.songsList.push(song);
-}
-//end Playlist class
 
-// Song Class
-function Song(data){
-  this.image=data.image;
-  this.songName=data.songTitle;
-  this.artistName=data.songAuthor;
-  this.duration=data.songLength;
-  this.count=data.songListened;
+//function Create playlists
+function createPlayLists(){
+  console.log(playListFromJS);
+  for (var i = 0; i < playListFromJS.length; i++) {
+    var bigPlaylist = new Playlist(playListFromJS[i]);
+    drawBigPlayList(bigPlaylist);
+  }
 }
-// end Song Class
+// end function create playlists
 
+//start draw and populate nodes --> playlist
+function drawBigPlayList(playlist){
+
+  var playlistDiv = document.getElementsByClassName('playListDivTemplate')[playlist.index];
+  playlistDiv.style.backgroundImage = 'url("'+playlist.imageLarge+'")';
+  playlistDiv.getElementsByTagName('span')[0] = playlist.playlistName;
+  playlistDiv.getElementsByTagName('span')[1] = playlist.description;
+}
+//end draw and populate nodes
 
 //start click explore playlist
 function myFunction(playlistNr){
+  var songsArray = [];
+  var selectedPlaylist = playListFromJS[playlistNr];
+  var playListObject = new Playlist(selectedPlaylist);
+  var draw = new Draw();
+  draw.beObserver(playListObject);
 
-  document.getElementById('playList').style.visibility="visible";
-  document.getElementById('playList').style.transition= "0.5s";
-  document.getElementById('playList').style.height="400px";
-
-  var songsArray=[];
-  var selectedPlaylist=playListFromJS[playlistNr];
-  var playListObject= new Playlist(selectedPlaylist);
-  playListObject.setName(selectedPlaylist.title);
-  console.log(selectedPlaylist.title);
-
-
-  for (var i = 0; i <  selectedPlaylist.songs.length; i++)
+  drawPlayListHeader(playListObject);
+  for (var i = 0; i < selectedPlaylist.songs.length; i++)
   {
-      var song= new Song(selectedPlaylist.songs[i]);
+      var song = new Song(selectedPlaylist.songs[i]);
       playListObject.addSong(song);
   }
 
-  console.log(playListObject);
 
-  for (var i = 0; i <  playListObject.songsList.length; i++)
-  {
-    var nr=i+1;
-    var div = document.getElementsByClassName("divToClone")[0];
-    var tempSong = playListObject.songsList[i];
-
-    clonedDiv = div.cloneNode(true);
-
-    clonedDiv.id = "song"+nr;
-    document.getElementById('playList').appendChild(clonedDiv);
-
-    document.getElementById('song'+nr).style.visibility='visible';
-    var imgPath =tempSong.image;
-    var img=clonedDiv.getElementsByTagName('img')[0];
-    img.style.float="left";
-    img.style.backgroundImage='url("'+imgPath+'")';
-
-    var tempDiv =clonedDiv.getElementsByTagName('div')[0];
-    tempDiv.style.textAlign="center";
-    tempDiv.style.width="50%";
-    tempDiv.style.display="inline-block";
-    var span=tempDiv.getElementsByTagName('span')[0];
-    span.innerText=tempSong.songName;
-
-    var tempDiv =clonedDiv.getElementsByTagName('div')[1];
-    tempDiv.style.textAlign="center";
-    tempDiv.style.width="10%";
-    tempDiv.style.display="inline-block";
-    var span=tempDiv.getElementsByTagName('span')[0];
-    span.innerText=convertToMin(tempSong.duration);
-
-    var tempDiv =clonedDiv.getElementsByTagName('div')[2];
-    tempDiv.style.textAlign="center";
-    tempDiv.style.width="20%";
-    tempDiv.style.display="inline-block";
-    var span=tempDiv.getElementsByTagName('span')[0];
-    span.innerText=tempSong.count;
-
-  }
 }
 //end click explore playlist
+
+//start draw songs in the playlist
+function drawPlayListHeader(playListObject) {
+
+  console.log(playListObject);
+  document.getElementById('playList').style.visibility = "visible";
+  document.getElementById('playList').style.transition = "0.5s";
+  document.getElementById('playList').style.height = "400px";
+
+  var playlistImg = document.getElementById('playListHeader').getElementsByTagName('img')[0];
+  console.log(playlistImg);
+  playlistImg.style.float = "left";
+  playlistImg.style.backgroundImage = 'url("'+playListObject.imageSmall+'")';
+  var titleSpan = document.getElementById('playListHeader').getElementsByTagName('span')[0];
+  titleSpan.innerText = playListObject.playlistName;
+}
+
+
+//end draw songs in the playlist
+
 
 function convertToMin(nr)
 {
   var min = Math.floor(nr / 60);
   var sec = nr % 60;
 
-  if (sec<10)
+  if (sec < 10)
   {
-    sec +="0";
+    sec += "0";
   }
-  var str = min+":"+sec;
+  var str = min + ":" + sec;
   return str;
 }
