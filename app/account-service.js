e658a522c9961737f1ec93653479cf7d9cae7cc0
Robert Cosma <robert.cosma@softvision.ro>
2016-03-22 16:50:30 +0200

    commit 1 day 7
    commit 2 day 7
    
    test pull request

--- app/account-service.js
+++ app/account-service.js
@@ -1,14 +1,34 @@
-var users=JSON.parse(usersJSON);
-
-
+var users = JSON.parse(usersJSON);
 
 function AccountService() {
-  
-}
-
-logIn = new AccountService(email,password) {
-
-  var  email = document.getElementById('email').value;
-  var  password = document.getElementById('password').value;
 
 }
+
+AccountService.prototype.checkLogin = function ( email , password ) {
+
+  var check = "0" ;
+
+  for ( i = 0 ; i < users.length ; i ++ ) {
+
+    if ( email == users[i].email && password == users[i].password ) {
+
+      return check = "1" ;
+
+      break ;
+
+    } else {
+
+        if ( email == users[i].email && password != users[i].password ) {
+
+            check = "2"
+
+            break;
+        }
+    }
+
+  }
+
+  return check;
+}
+
+var accountService = new AccountService() ;
--- app/song-playlist.js
+++ app/song-playlist.js
@@ -1,16 +1,39 @@
-var list = JSON.parse(playlistsJSON);
-
 function Song(data) {
 
   this.image = data.image;
-  this.Title = data.songTitle;
-  this.Author = data.songAuthor;
-  this.Length = data.songLength;
-  this.Listened = data.songListened;
+  this.songTitle = data.songTitle;
+  this.songAuthor = data.songAuthor;
+  this.songLength = data.songLength;
+  this.songListened = data.songListened;
 
 }
 
-function Playlist() {
+Song.prototype.getImage = function () {
+
+  return this.image ;
+}
+
+Song.prototype.getSongTitle = function () {
+
+  return this.songTitle;
+}
+
+Song.prototype.getSongAuthor = function () {
+
+  return this.songAuthor;
+}
+
+Song.prototype.getSongLength = function () {
+
+  return this.songLength;
+}
+
+Song.prototype.getSongListened = function () {
+
+  return this.songListened;
+}
+
+/*function Playlist() {
 
   this.name = title;
   this.songList = [];
@@ -21,4 +44,4 @@ function Playlist() {
 Playlist.prototype.addSong = function (song) {
   this.songList.push(song);
 
-}
+} */
