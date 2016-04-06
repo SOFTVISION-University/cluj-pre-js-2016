9d86615d5f3ce3e82417d690fdcde05e15315662
Luca Marius <Luca Marius>
2016-04-06 03:15:42 +0300

    Implement router
    Implement router for home page and login page
    Create home-page-view

--- app/app_views/footer-view.js
+++ app/app_views/footer-view.js
@@ -1,10 +1,4 @@
 const FooterView = Backbone.View.extend({
-  copyright() {
-    const date = new Date();
-    const year = date.getFullYear();
-    document.getElementById('copyrighthome').innerHTML = `&copy; ${year} ACME
-    DIVISION ALL RIGHTS RESERVED`;
-  },
   render() {
     const template = _.template($('#template-FooterView').html(), {});
     this.$el.html(template);
--- app/entry.js
+++ app/entry.js
@@ -1,30 +1,4 @@
-import { HeaderView } from './app_views/header-view.js';
-import { ContentView } from './app_views/content-view.js';
-import { FooterView } from './app_views/footer-view.js';
-import { PlaylistCollection } from './collections/playlist-collection.js';
-import { PlaylistsView } from './views/playlists-view.js';
-import { UserModel } from './models/user-model.js';
-import { UserView } from './views/user-view.js';
+import { AppRouter } from './routers/router.js';
 
-const headerView = new HeaderView({ el: $('#index-header') });
-headerView.render();
-const contentView = new ContentView({ el: $('#index-content') });
-contentView.render();
-const playlists = new PlaylistCollection();
-playlists.fetch().done(() => {
-  const playlistsView = new PlaylistsView({
-    el: document.getElementById('index-card-area'),
-    collection: playlists,
-  });
-  playlistsView.render();
-});
-const footerView = new FooterView({ el: $('#footer') });
-footerView.render();
-footerView.copyright();
-
-const userModel = new UserModel({});
-const userView = new UserView({
-  el: document.getElementById('authentication'),
-  model: userModel,
-});
-userView.render();
+const appRouter = new AppRouter();
+Backbone.history.start();
